9850f64841f0a7190170e27f9a7e2b0e9b2c85cf
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-07-28 11:46:03 +0100

    final files

--- index.js
+++ index.js
@@ -40,3 +40,15 @@ const colors = [
   "amber",
   "mint",
 ];
+
+let btn = document.querySelector(".btn");
+let color = document.querySelector(".color");
+btn.addEventListener("click", () => {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
